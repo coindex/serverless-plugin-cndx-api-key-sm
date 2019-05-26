4c0e4bc7bc5c32867916017a80f52bc38072acaf
Sergio Martins Pereira <s@x42.io>
2019-05-26 11:40:28 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # serverless-plugin-cndx-api-key-sm
+
 serverless plugin to export a selected api gateway api key to a selected sm secret
---
+++ plugin.js
@@ -0,0 +1,53 @@
+'use strict'
+
+class APIKeySMPlugin {
+
+  constructor(serverless, options) {
+    this.serverless = serverless
+    this.options = options || {}
+    this.provider = this.serverless.getProvider('aws')
+    this.stage = this.options.stage || this.serverless.service.provider.stage
+
+    this.hooks = { 'after:deploy:deploy': this.exportApiKey.bind(this) }
+  }
+
+  async exportApiKey() {
+    this.serverless.cli.log('cndx-api-key-sm-plugin starting...')
+
+    this.serverless.cli.log('getting stack...')
+    const stack = (await this.provider.request(
+      'CloudFormation', 'describeStacks',
+      { StackName: this.provider.naming.getStackName(this.stage) }
+    )).Stacks[0]
+
+    this.serverless.cli.log('getting stack outputs...')
+    const stackOutputs = stack.Outputs.reduce(
+      (os, o) => ({ ...os, ...{ [o.OutputKey]: o.OutputValue } }), {}
+    )
+
+    this.serverless.cli.log('processing stack outputs...')
+    const {
+      AWSApiGatewayApiKeyId,
+      AWSSecretsManagerSecretAPIArn
+    } = stackOutputs
+
+    this.serverless.cli.log('getting api key value...')
+    const apiKeyValue = (await this.provider.request(
+      'APIGateway', 'getApiKey',
+      { apiKey: AWSApiGatewayApiKeyId, includeValue: true }
+    )).value
+
+    this.serverless.cli.log('exporting api key value...')
+    await this.provider.request(
+      'SecretsManager', 'putSecretValue',
+      {
+        SecretId: AWSSecretsManagerSecretAPIArn,
+        SecretString: apiKeyValue
+      }
+    )
+
+    this.serverless.cli.log('cndx-api-key-sm-plugin finished...')
+  }
+}
+
+module.exports = APIKeySMPlugin
